365348a5c9a1e47881262b92856fde5844791285
royal2710 <royal2710@outlook.com>
2021-02-28 01:45:16 +0800

    scheduling example

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Log;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Test Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Log::info('=== Hello World! - Test Command ===');
+        return 0;
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,9 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
+use Log;
+use App\Console\Commands\SendEmailsCommand;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,6 +28,12 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            // DB::table('recent_users')->delete();
+            Log::info('=== Hello World! - Cron Job ===');
+        })->everyMinute();
+        //
+        $schedule->command('test:command')->everyMinute();
     }
 
     /**
